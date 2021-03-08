22ba8e31c11d011efc8748588ff8b08117db6f1e
nicku12345 <68750811+nicku12345@users.noreply.github.com>
2021-03-08 15:59:22 +0800

    8 mar

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from config.RemoteTickerCounter import RTC
 import time
 
 while True:
+    t = time.time()
+
     print("-------------------------------------")
     print("scanning...", end = "\r", flush = True)
     RTC.Query(num_thread = 60, num_reply = 60)
+
+    dt = round(time.time() - t, 4)
+    print("\n" + f"{dt} seconds used...")
+
     print("\n" + f"Top {20} discussed tickers:")
     RTC.display(20)
 
